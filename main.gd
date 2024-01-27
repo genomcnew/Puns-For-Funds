94168a7dbdf01291c898514c03b9f9951f0d8e24
Geno McNew <genomcnew@gmail.com>
2024-01-27 13:03:26 -0600

    WIP

--- main.gd
+++ main.gd
@@ -3,10 +3,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Dialogic.start('timeline')
+	#Dialogic.start('timeline')
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_house_button_pressed():
+	Dialogic.start("timeline")
+	pass # Replace with function body.
