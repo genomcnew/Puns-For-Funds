7866d6bc10ae1f523ff3a5be3d04d08f1efb238d
Geno McNew <genomcnew@gmail.com>
2024-01-27 14:04:01 -0600

    wip

--- main.gd
+++ main.gd
@@ -13,5 +13,15 @@ func _process(delta):
 
 
 func _on_house_button_pressed():
-	Dialogic.start("timeline")
+	#Dialogic.start("timeline")
+	#change scene in house
+	pass # Replace with function body.
+
+
+func _on_bar_pressed():
+	#change scene to bar
+	pass # Replace with function body.
+
+
+func _on_improv_pressed():
 	pass # Replace with function body.
